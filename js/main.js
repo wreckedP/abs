f715752d8d5b61282f893f5bf9f7bad8e936b481
Svan-derp <60352754+Svan-derp@users.noreply.github.com>
2023-01-16 17:02:03 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,18 +1,16 @@
 // Load load "components"
-fetch('navbar.html')
-.then(res => res.text())
-.then(text => {
-    let oldelem = document.querySelector("script#navbar");
-    let newelem = document.createElement("div");
-    newelem.innerHTML = text;
-    oldelem.parentNode.replaceChild(newelem,oldelem);
-})
 
-fetch('footer.html')
+const compontents = ["navbar","footer"]
+
+function loadHTML(tag) {
+fetch(tag + ".html")
 .then(res => res.text())
 .then(text => {
-    let oldelem = document.querySelector("script#footer");
+    let oldelem = document.querySelector("script#" + tag);
     let newelem = document.createElement("div");
     newelem.innerHTML = text;
     oldelem.parentNode.replaceChild(newelem,oldelem);
 })
+}
+
+compontents.forEach(n => loadHTML(n))
